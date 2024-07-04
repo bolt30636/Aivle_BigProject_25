c0ac67b10c73163e976404779df9a3ec13db5657
이혜원 <150877811+bolt30636@users.noreply.github.com>
2024-07-04 14:15:00 +0900

    Add files via upload

---
+++ BigProject_25/BigProject_25/src/main/java/com/example/BigProject_25/BigProject25Application.java
@@ -0,0 +1,13 @@
+package com.example.BigProject_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigProject25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigProject25Application.class, args);
+	}
+
+}
