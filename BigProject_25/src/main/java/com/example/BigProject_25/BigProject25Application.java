d236959e0d814b18fd15716cf429db72785e924e
MinkyeongKang <93696682+mink0517@users.noreply.github.com>
2024-07-10 10:53:26 +0900

    7월 10일 민경 수정해봄

--- BigProject_25/src/main/java/com/example/BigProject_25/BigProject25Application.java
+++ BigProject_25/src/main/java/com/example/BigProject_25/BigProject25Application.java
@@ -3,6 +3,7 @@ package com.example.BigProject_25;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*20240710*/
+/*0710민경 오전 수정*/
 @SpringBootApplication
 public class BigProject25Application {
 
